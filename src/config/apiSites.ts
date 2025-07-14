1487e4dd12b940df5d6cc5be8673a986ca35e90a
Webster <472680811@qq.com>
2025-07-14 16:42:11 +0800

    Update apiSites.ts
    add site

--- src/config/apiSites.ts
+++ src/config/apiSites.ts
@@ -19,4 +19,95 @@ export const API_SITES = [
     api: "http://ffzy5.tv",
     name: "非凡影视"
   },
+  {
+      key: 'yttzy',
+      api: 'https://caiji.dyttzyapi.com/api.php/provide/vod',
+      name: '电影天堂资源',
+  },
+  {
+      key: 'ruyi',
+      api: 'https://cj.rycjapi.com/api.php/provide/vod',
+      name: '如意资源',
+  },
+  {
+      key: 'bfzy',
+      api: 'https://bfzyapi.com/api.php/provide/vod',
+      name: '暴风资源',
+  },
+  {
+      key: 'tyyszy',
+      api: 'https://tyyszy.com/api.php/provide/vod',
+      name: '天涯资源',
+  },
+  {
+      key: 'xiaomaomi',
+      api: 'https://zy.xiaomaomi.cc/api.php/provide/vod',
+      name: '小猫咪资源',
+  },
+  {
+      key: 'zy360',
+      api: 'https://360zy.com/api.php/provide/vod',
+      name: '360资源',
+  },
+  {
+      key: 'iqiyi',
+      api: 'https://www.iqiyizyapi.com/api.php/provide/vod',
+      name: 'iqiyi资源',
+  },
+  {
+      key: 'wolong',
+      api: 'https://wolongzyw.com/api.php/provide/vod',
+      name: '卧龙资源',
+  }, 
+  {
+      key: 'hwba',
+      api: 'https://cjhwba.com/api.php/provide/vod',
+      name: '华为吧资源',
+  },
+  {
+      key: 'jisu',
+      api: 'https://jszyapi.com/api.php/provide/vod',
+      name: '极速资源',
+      detail: 'https://jszyapi.com', 
+  },
+  {
+      key: 'dbzy',
+      api: 'https://dbzy.tv/api.php/provide/vod',
+      name: '豆瓣资源',
+  },
+  {
+      key: 'mozhua',
+      api: 'https://mozhuazy.com/api.php/provide/vod',
+      name: '魔爪资源',
+  },
+  {
+      key: 'mdzy',
+      api: 'https://www.mdzyapi.com/api.php/provide/vod',
+      name: '魔都资源',
+  },
+  {
+      key: 'zuid',
+      api: 'https://api.zuidapi.com/api.php/provide/vod',
+      name: '最大资源'
+  },
+  {
+      key: 'yinghua',
+      api: 'https://m3u8.apiyhzy.com/api.php/provide/vod',
+      name: '樱花资源'
+  },
+  {
+      key: 'baidu',
+      api: 'https://api.apibdzy.com/api.php/provide/vod',
+      name: '百度云资源'
+  },
+  {
+      key: 'wujin',
+      api: 'https://api.wujinapi.me/api.php/provide/vod',
+      name: '无尽资源'
+  },
+  {
+      key: 'wwzy',
+      api: 'https://wwzy.tv/api.php/provide/vod',
+      name: '旺旺短剧'
+  },
 ];
