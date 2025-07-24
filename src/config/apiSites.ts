52e6c813fc9349821f4c1a0f7eb7fbef9dec4e44
Webster <472680811@qq.com>
2025-07-24 16:53:57 +0800

    Update apiSites.ts

--- src/config/apiSites.ts
+++ src/config/apiSites.ts
@@ -5,11 +5,6 @@ export const API_SITES = [
     name: "黑木耳",
   },
   {
-    key: "mdzy",
-    api: "https://www.mdzyapi.com",
-    name: "魔都资源",
-  },
-  {
     key: "wolong",
     api: "https://wolongzyw.com",
     name: "卧龙资源",
@@ -26,103 +21,103 @@ export const API_SITES = [
   },
   {
     key: 'yttzy',
-    api: 'https://caiji.dyttzyapi.com/api.php/provide/vod',
+    api: 'https://caiji.dyttzyapi.com',
     name: '电影天堂资源',
   },
   {
     key: 'ruyi',
-    api: 'https://cj.rycjapi.com/api.php/provide/vod',
+    api: 'https://cj.rycjapi.com',
     name: '如意资源',
   },
   {
     key: 'bfzy',
-    api: 'https://bfzyapi.com/api.php/provide/vod',
+    api: 'https://bfzyapi.com',
     name: '暴风资源',
   },
   {
     key: 'tyyszy',
-    api: 'https://tyyszy.com/api.php/provide/vod',
+    api: 'https://tyyszy.com',
     name: '天涯资源',
   },
   // {
   //     key: 'xiaomaomi',
-  //     api: 'https://zy.xiaomaomi.cc/api.php/provide/vod',
+  //     api: 'https://zy.xiaomaomi.cc',
   //     name: '小猫咪资源',
   // },
   {
     key: 'zy360',
-    api: 'https://360zy.com/api.php/provide/vod',
+    api: 'https://360zy.com',
     name: '360资源',
   },
   // {
   //     key: 'iqiyi',
-  //     api: 'https://www.iqiyizyapi.com/api.php/provide/vod',
+  //     api: 'https://www.iqiyizyapi.com',
   //     name: 'iqiyi资源',
   // },
   {
     key: 'wolong',
-    api: 'https://wolongzyw.com/api.php/provide/vod',
+    api: 'https://wolongzyw.com',
     name: '卧龙资源',
   }, 
   // {
   //     key: 'hwba',
-  //     api: 'https://cjhwba.com/api.php/provide/vod',
+  //     api: 'https://cjhwba.com',
   //     name: '华为吧资源',
   // },
   {
     key: 'jisu',
-    api: 'https://jszyapi.com/api.php/provide/vod',
+    api: 'https://jszyapi.com',
     name: '极速资源',
     detail: 'https://jszyapi.com', 
   },
   {
     key: 'dbzy',
-    api: 'https://dbzy.tv/api.php/provide/vod',
+    api: 'https://dbzy.tv',
     name: '豆瓣资源',
   },
   {
     key: 'mozhua',
-    api: 'https://mozhuazy.com/api.php/provide/vod',
+    api: 'https://mozhuazy.com',
     name: '魔爪资源',
   },
   {
     key: 'mdzy',
-    api: 'https://www.mdzyapi.com/api.php/provide/vod',
+    api: 'https://www.mdzyapi.com',
     name: '魔都资源',
   },
   {
     key: 'zuid',
-    api: 'https://api.zuidapi.com/api.php/provide/vod',
+    api: 'https://api.zuidapi.com',
     name: '最大资源'
   },
   // {
   //     key: 'yinghua',
-  //     api: 'https://m3u8.apiyhzy.com/api.php/provide/vod',
+  //     api: 'https://m3u8.apiyhzy.com',
   //     name: '樱花资源'
   // },
   {
     key: 'baidu',
-    api: 'https://api.apibdzy.com/api.php/provide/vod',
+    api: 'https://api.apibdzy.com',
     name: '百度云资源'
   },
   {
     key: 'wujin',
-    api: 'https://api.wujinapi.me/api.php/provide/vod',
+    api: 'https://api.wujinapi.me',
     name: '无尽资源'
   },
   {
     key: 'wwzy',
-    api: 'https://wwzy.tv/api.php/provide/vod',
+    api: 'https://wwzy.tv',
     name: '旺旺短剧'
   },
   {
     key: 'mtzy',
-    api: 'https://mtzy.me/api.php/provide/vod',
+    api: 'https://mtzy.me',
     name: '茅台资源'
   },
   {
     key: 'qilin',
-    api: 'https://www.qilinzyz.com/api.php/provide/vod',
+    api: 'https://www.qilinzyz.com',
     name: '麒麟资源'
   },
 ];
